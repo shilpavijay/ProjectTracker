b4523fec6c072d1c89ecac410eb982f8df6480a5
Shilpa AB <shilpa.amberker@gmail.com>
2022-02-19 21:16:23 +0530

    Navigation component

---
+++ src/Components/Navigation.js
@@ -0,0 +1,152 @@
+import React from "react";
+import Divider from "@material-ui/core/Divider";
+import {
+  Navbar,
+  NavDropdown,
+  Nav,
+  Container,
+  Button,
+  Form,
+  FormControl,
+} from "react-bootstrap";
+import "./navigation.css";
+import NotificationsNoneRoundedIcon from "@mui/icons-material/NotificationsNoneRounded";
+
+function Navigation() {
+  return (
+    <div>
+      <Navbar bg="white" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Project Tracker</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavDropdown
+                title="Your Work"
+                id="basic-nav-dropdown"
+                style={{ fontSize: 14 }}
+              >
+                <NavDropdown.Item href="#action/3.1" style={{ fontSize: 14 }}>
+                  Action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" style={{ fontSize: 14 }}>
+                  Another action
+                </NavDropdown.Item>
+
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4" style={{ fontSize: 14 }}>
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown
+                title="Projects"
+                id="basic-nav-dropdown"
+                style={{ fontSize: 14 }}
+              >
+                <NavDropdown.Item href="#action/3.1" style={{ fontSize: 14 }}>
+                  Action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" style={{ fontSize: 14 }}>
+                  Another action
+                </NavDropdown.Item>
+
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4" style={{ fontSize: 14 }}>
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown
+                title="Filters"
+                id="basic-nav-dropdown"
+                style={{ fontSize: 14 }}
+              >
+                <NavDropdown.Item href="#action/3.1" style={{ fontSize: 14 }}>
+                  Action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" style={{ fontSize: 14 }}>
+                  Another action
+                </NavDropdown.Item>
+
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4" style={{ fontSize: 14 }}>
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown
+                title="Dashboards"
+                id="basic-nav-dropdown"
+                style={{ fontSize: 14 }}
+              >
+                <NavDropdown.Item href="#action/3.1" style={{ fontSize: 14 }}>
+                  Action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" style={{ fontSize: 14 }}>
+                  Another action
+                </NavDropdown.Item>
+
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4" style={{ fontSize: 14 }}>
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown
+                title="People"
+                id="basic-nav-dropdown"
+                style={{ fontSize: 14 }}
+              >
+                <NavDropdown.Item href="#action/3.1" style={{ fontSize: 14 }}>
+                  Action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" style={{ fontSize: 14 }}>
+                  Another action
+                </NavDropdown.Item>
+
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4" style={{ fontSize: 14 }}>
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown
+                title="Apps  "
+                id="basic-nav-dropdown"
+                style={{ fontSize: 14 }}
+              >
+                <NavDropdown.Item href="#action/3.1" style={{ fontSize: 14 }}>
+                  Action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" style={{ fontSize: 14 }}>
+                  Another action
+                </NavDropdown.Item>
+
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4" style={{ fontSize: 14 }}>
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Button variant="primary" size="sm">
+                Create
+              </Button>{" "}
+            </Nav>
+          </Navbar.Collapse>
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="primary" size="sm">
+              Search
+            </Button>
+          </Form>
+          <button className="iconbutton">
+            <NotificationsNoneRoundedIcon />
+          </button>
+        </Container>
+      </Navbar>
+      <Divider className="divider" />
+    </div>
+  );
+}
+
+export default Navigation;
---
+++ src/loginPage.js
@@ -0,0 +1,39 @@
+<Container>
+  <h1 className="title">{"Project Tracker"}</h1>
+  <Row>
+    <Col>{<img src={logo} className="App-logo" alt="logo" />}</Col>
+    <Col xs={5}>
+      <Form>
+        <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+          <Form.Label column sm={2}>
+            Email
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="email" placeholder="Email" />
+          </Col>
+        </Form.Group>
+        <Form.Group
+          as={Row}
+          className="mb-3"
+          controlId="formHorizontalPassword"
+        >
+          <Form.Label column sm={2}>
+            Password
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="password" placeholder="Password" />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3" controlId="formHorizontalCheck">
+          <Col sm={{ span: 10, offset: 2 }}>
+            <Form.Check label="Remember me" />
+          </Col>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Col>
+  </Row>
+</Container>;
